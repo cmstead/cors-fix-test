f5886625f329e5961b745354c4013ab7542ff981
Chris Stead <cmstead@gmail.com>
2017-10-20 20:39:19 -0700

    Updated domain to https

--- csproxy.js
+++ csproxy.js
@@ -1,12 +1,12 @@
 var csProxyUtils = (function () {
     function buildProxyUrl(apiKey, requestUrl) {
-        var hostUri = 'www.chrisstead.com/proxy/';
-        return 'http://' + hostUri + '?apikey=' + apiKey + '&requesturl=' + requestUrl;
+        var hostUri = 'https://www.chrisstead.com/proxy/';
+        return hostUri + '?apikey=' + apiKey + '&requesturl=' + requestUrl;
     }
     
     function buildJsonpProxyUrl(apiKey, requestUrl) {
-        var hostUri = 'www.chrisstead.com/proxy/jsonp.php';
-        return 'http://' + hostUri + '?apikey=' + apiKey + '&requesturl=' + requestUrl;
+        var hostUri = 'https://www.chrisstead.com/proxy/jsonp.php';
+        return hostUri + '?apikey=' + apiKey + '&requesturl=' + requestUrl;
     }
     
     return {
