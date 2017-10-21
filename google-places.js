abd4262e842d3fb88d7ca40a0c86a60c409bcc29
Chris Stead <cmstead@gmail.com>
2017-10-20 20:57:49 -0700

    Updated to match remote reference script

--- google-places.js
+++ google-places.js
@@ -1,35 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var placesApiKey = 'AIzaSyA4Fw5m2LqG8i8NYsdx4NgIY_PGB0ouA5s';
     var placesBaseUri = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=' + placesApiKey;
 
     var placesTestUri = placesBaseUri + '&location=32.7157,-117.1611&radius=5000';
 
-    // console.log(placesTestUri);
-
-    // var proxyUri = 'www.chrisstead.com/proxy/';
-    // var proxyUri = 'localhost/simphpleProxy/';
-    var proxyUri = 'localhost/simphpleProxy/jsonp.php';
     var proxyKey = '55d9430e09095b44d75ece0c0380c9daf1946332';
 
-    var requestUri = `http://${proxyUri}?apikey=${proxyKey}&requesturl=${placesTestUri}`;
-
-    // console.log(requestUri);
-
-    framejax
-        .ajax({
-            url: requestUri
-        })
-        .done(function(data) {
-            console.log('Yay, it worked!');
-            console.log(JSON.stringify(data));
-        });
-
-    // $.ajax({
-    //     method: 'GET',
-    //     url: requestUri
-    // }).done(function (result) {
-    //     console.log('Yay!');
-    //     console.log(result);
-    // });
-
+    $.ajax({
+        url: csProxyUtils.buildProxyUrl(proxyKey, placesTestUri)
+    }).done(function (data) {
+        console.log('Yay, it worked!');
+        console.log(JSON.stringify(data));
+    });
 });
